8edf67c6628ca64a63058815027755f7fa2079f6
DanzPodz <daniel.uberall8@gmail.com>
2023-11-14 11:42:41 +0000

    updated password and username

---
+++ Main.py
@@ -0,0 +1,73 @@
+import os
+import logging
+from datetime import timedelta
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_wtf.csrf import CSRFProtect
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+csrf = CSRFProtect(app)
+app.config['SECRET_KEY'] = os.environ.get('FLASK_SECRET_KEY') or 'replace_with_secure_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=1)  # Set the session timeout
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+logging.basicConfig(level=logging.DEBUG)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+@app.route('/')
+def home():
+    return "Welcome to the Home Page"
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        new_user = User(username=username)
+        new_user.set_password(password)
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            logging.error(f"An error occurred: {str(e)}")
+        return redirect(url_for('login'))
+    return render_template('signup.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.check_password(password):
+            session['user_id'] = user.id
+            return redirect(url_for('dashboard'))
+        else:
+            return "Login failed. Please check your credentials."
+    return render_template('login.html')
+
+@app.route('/dashboard')
+def dashboard():
+    user_id = session.get('user_id')
+    if user_id:
+        return f"Welcome to the Dashboard, User {user_id}!"
+    return "You are not logged in."
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
